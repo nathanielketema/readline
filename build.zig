8a8830827946e5116bca3041619995fa0b810c0c
Nathaniel Ketema <nathanielketema@gmail.com>
2026-06-14 21:30:39 -0500

    use zig's addCMacro instead of -D

--- build.zig
+++ build.zig
@@ -21,6 +21,81 @@ pub fn build(b: *std.Build) void {
     mod.addIncludePath(upstream_root);
     mod.linkSystemLibrary("ncurses", .{});
 
+    mod.addCMacro("_DARWIN_C_SOURCE", "1");
+    mod.addCMacro("_POSIX_VERSION", "200112L");
+    mod.addCMacro("HAVE_UNISTD_H", "1");
+    mod.addCMacro("HAVE_STDLIB_H", "1");
+    mod.addCMacro("HAVE_STRING_H", "1");
+    mod.addCMacro("HAVE_STRINGS_H", "1");
+    mod.addCMacro("HAVE_DIRENT_H", "1");
+    mod.addCMacro("HAVE_FCNTL_H", "1");
+    mod.addCMacro("HAVE_WCHAR_H", "1");
+    mod.addCMacro("HAVE_WCTYPE_H", "1");
+    mod.addCMacro("HAVE_LOCALE_H", "1");
+    mod.addCMacro("HAVE_LIMITS_H", "1");
+    mod.addCMacro("HAVE_STDARG_H", "1");
+    mod.addCMacro("HAVE_STDBOOL_H", "1");
+    mod.addCMacro("HAVE_SYS_IOCTL_H", "1");
+    mod.addCMacro("HAVE_SYS_TIME_H", "1");
+    mod.addCMacro("HAVE_SYS_SELECT_H", "1");
+    mod.addCMacro("HAVE_SYS_FILE_H", "1");
+    mod.addCMacro("HAVE_TERMIOS_H", "1");
+    mod.addCMacro("HAVE_PWD_H", "1");
+    mod.addCMacro("HAVE_LANGINFO_H", "1");
+    mod.addCMacro("HAVE_MEMORY_H", "1");
+    mod.addCMacro("HAVE_CHOWN", "1");
+    mod.addCMacro("HAVE_FCNTL", "1");
+    mod.addCMacro("HAVE_FNMATCH", "1");
+    mod.addCMacro("HAVE_GETPWENT", "1");
+    mod.addCMacro("HAVE_GETPWNAM", "1");
+    mod.addCMacro("HAVE_GETPWUID", "1");
+    mod.addCMacro("HAVE_GETTIMEOFDAY", "1");
+    mod.addCMacro("HAVE_ISASCII", "1");
+    mod.addCMacro("HAVE_ISXDIGIT", "1");
+    mod.addCMacro("HAVE_KILL", "1");
+    mod.addCMacro("HAVE_LSTAT", "1");
+    mod.addCMacro("HAVE_MBRLEN", "1");
+    mod.addCMacro("HAVE_MBRTOWC", "1");
+    mod.addCMacro("HAVE_MEMMOVE", "1");
+    mod.addCMacro("HAVE_PSELECT", "1");
+    mod.addCMacro("HAVE_PUTENV", "1");
+    mod.addCMacro("HAVE_READLINK", "1");
+    mod.addCMacro("HAVE_SELECT", "1");
+    mod.addCMacro("HAVE_SETENV", "1");
+    mod.addCMacro("HAVE_SETITIMER", "1");
+    mod.addCMacro("HAVE_SETLOCALE", "1");
+    mod.addCMacro("HAVE_STRCASECMP", "1");
+    mod.addCMacro("HAVE_STRCOLL", "1");
+    mod.addCMacro("HAVE_STRPBRK", "1");
+    mod.addCMacro("HAVE_SYSCONF", "1");
+    mod.addCMacro("HAVE_TCGETATTR", "1");
+    mod.addCMacro("HAVE_VSNPRINTF", "1");
+    mod.addCMacro("HAVE_WCRTOMB", "1");
+    mod.addCMacro("HAVE_WCSCOLL", "1");
+    mod.addCMacro("HAVE_WCSLEN", "1");
+    mod.addCMacro("HAVE_WCSNLEN", "1");
+    mod.addCMacro("HAVE_WCSNRTOMBS", "1");
+    mod.addCMacro("HAVE_WCTYPE", "1");
+    mod.addCMacro("HAVE_WCWIDTH", "1");
+    mod.addCMacro("HAVE_MBSTATE_T", "1");
+    mod.addCMacro("HAVE_WCHAR_T", "1");
+    mod.addCMacro("HAVE_WCTYPE_T", "1");
+    mod.addCMacro("HAVE_WINT_T", "1");
+    mod.addCMacro("HAVE_ISWCTYPE", "1");
+    mod.addCMacro("HAVE_ISWLOWER", "1");
+    mod.addCMacro("HAVE_ISWUPPER", "1");
+    mod.addCMacro("HAVE_TOWLOWER", "1");
+    mod.addCMacro("HAVE_TOWUPPER", "1");
+    mod.addCMacro("HAVE_LANGINFO_CODESET", "1");
+    mod.addCMacro("HAVE_POSIX_SIGNALS", "1");
+    mod.addCMacro("HAVE_POSIX_SIGSETJMP", "1");
+    mod.addCMacro("GWINSZ_IN_SYS_IOCTL", "1");
+    mod.addCMacro("STRUCT_WINSIZE_IN_SYS_IOCTL", "1");
+    mod.addCMacro("HAVE_GETPW_DECLS", "1");
+    mod.addCMacro("HAVE_STRUCT_DIRENT_D_INO", "1");
+    mod.addCMacro("HAVE_STRUCT_DIRENT_D_FILENO", "1");
+    mod.addCMacro("HAVE_TIMEVAL", "1");
+
     mod.addCSourceFiles(.{
         .root = upstream_root,
         .files = &.{
@@ -64,82 +139,11 @@ pub fn build(b: *std.Build) void {
             "-Wno-format-security",
             "-DBRACKETED_PASTE_DEFAULT=1",
             "-DRL_LIBRARY_VERSION=\"8.3\"",
-            "-DHAVE_UNISTD_H",
-            "-DHAVE_STDLIB_H",
-            "-DHAVE_STRING_H",
-            "-DHAVE_STRINGS_H",
-            "-DHAVE_DIRENT_H",
-            "-DHAVE_FCNTL_H",
-            "-DHAVE_WCHAR_H",
-            "-DHAVE_WCTYPE_H",
-            "-DHAVE_LOCALE_H",
-            "-DHAVE_LIMITS_H",
-            "-DHAVE_STDARG_H",
-            "-DHAVE_STDBOOL_H",
-            "-DHAVE_SYS_IOCTL_H",
-            "-DHAVE_SYS_TIME_H",
-            "-DHAVE_SYS_SELECT_H",
-            "-DHAVE_TERMIOS_H",
-            "-DHAVE_PWD_H",
-            "-DHAVE_LANGINFO_H",
-            "-DHAVE_MEMORY_H",
-            "-DHAVE_CHOWN",
-            "-DHAVE_FCNTL",
-            "-DHAVE_FNMATCH",
-            "-DHAVE_GETPWENT",
-            "-DHAVE_GETPWNAM",
-            "-DHAVE_GETPWUID",
-            "-DHAVE_GETTIMEOFDAY",
-            "-DHAVE_ISASCII",
-            "-DHAVE_KILL",
-            "-DHAVE_LSTAT",
-            "-DHAVE_MBRLEN",
-            "-DHAVE_MBRTOWC",
-            "-DHAVE_MEMMOVE",
-            "-DHAVE_PSELECT",
-            "-DHAVE_PUTENV",
-            "-DHAVE_READLINK",
-            "-DHAVE_SELECT",
-            "-DHAVE_SETENV",
-            "-DHAVE_SETITIMER",
-            "-DHAVE_SETLOCALE",
-            "-DHAVE_STRCASECMP",
-            "-DHAVE_STRCOLL",
-            "-DHAVE_STRPBRK",
-            "-DHAVE_SYSCONF",
-            "-DHAVE_TCGETATTR",
-            "-DHAVE_VSNPRINTF",
-            "-DHAVE_WCRTOMB",
-            "-DHAVE_WCSCOLL",
-            "-DHAVE_WCSLEN",
-            "-DHAVE_WCSNLEN",
-            "-DHAVE_WCSNRTOMBS",
-            "-DHAVE_WCTYPE",
-            "-DHAVE_WCWIDTH",
-            "-DHAVE_MBSTATE_T",
-            "-DHAVE_WCHAR_T",
-            "-DHAVE_WCTYPE_T",
-            "-DHAVE_WINT_T",
-            "-DHAVE_ISWCTYPE",
-            "-DHAVE_ISWLOWER",
-            "-DHAVE_ISWUPPER",
-            "-DHAVE_TOWLOWER",
-            "-DHAVE_TOWUPPER",
-            "-DHAVE_LANGINFO_CODESET",
-            "-DHAVE_POSIX_SIGNALS",
-            "-DHAVE_POSIX_SIGSETJMP",
-            "-DGWINSZ_IN_SYS_IOCTL",
-            "-DSTRUCT_WINSIZE_IN_TERMIOS",
-            "-DHAVE_GETPW_DECLS",
-            "-DHAVE_STRUCT_DIRENT_D_INO",
-            "-DHAVE_STRUCT_DIRENT_D_FILENO",
             "-DREADLINE_LIBRARY",
-            "-D_POSIX_VERSION 200112L",
-            "-DHAVE_TIMEVAL",
         },
     });
 
-    lib.installHeadersDirectory(upstream_root, "", .{
+    lib.installHeadersDirectory(upstream_root, "readline", .{
         .include_extensions = &.{
             "chardefs.h",
             "history.h",
